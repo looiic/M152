7373411a5a93033d03db1e1287481692bba28b13
looiic <loic.jermann@gibmit.ch>
2018-11-26 20:19:33 +0100

    Song upload works

--- server/server.js
+++ server/server.js
@@ -1,10 +1,87 @@
-var express = require('express');
-var app = express();
+/**
+ * NPM Module dependencies.
+ */
+const express = require('express');
+const trackRoute = express.Router();
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/db');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const app = express();
+app.use('/song', trackRoute);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+// var storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads/')
+//     },
+//     filename: function (req, file, cb) {
+//         var songName = Date.now() + '.mp3';
+//         cb(null, songName)
+//     }
+// });
+//
+// var upload = multer({ storage: storage }).single('song');
+//
+// trackRoute.post('/', function (req, res) {
+//     upload(req, res, function (err) {
+//         if (err) {
+//             // An error occurred when uploading
+//             console.log(err);
+//         }
+//         res.json({
+//             success: true,
+//             message: 'Song uploaded!'
+//         });
+//
+//         // Everything went fine
+//     })
+// });
+
+var Song = mongoose.model('Song', {
+    titel: {
+      type: String
+    },
+    interpret: {
+      type: String
+    },
+    song: {
+      type: String
+    }
+})
+
+trackRoute.post('/', upload.any(), function(req,res,next){
+
+  if(req.files){
+    req.files.forEach(function(file){
+      var filename = Date.now() + '.mp3';
+      fs.rename(file.path, 'uploads/' + filename, function (err){
+        if(err)throw err;
+
+        var song = new Song({
+          titel: req.body.titel,
+          interpret: req.body.interpret,
+          song: filename
+        });
+
+        song.save(function(err, result){
+          if(err){
+
+          }
+          res.json(result);
+        })
+      })
+    })
+
+
+  }
+
+
+})
+
+
+app.listen(3005, () => {
+  console.log("App listening on port 3005!");
 });
